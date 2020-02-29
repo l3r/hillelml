c77322050e96cee9e5bb2e81f2be575677e6adb2
l3r <sergey.s@myco.com>
2020-02-29 14:04:59 +0200

    task3

--- main.py
+++ main.py
@@ -2,7 +2,12 @@
 #print("hello, {}!".format(name))
 
 
-a = int(input("Enter 1st num:"))
-b = int(input("Enter 2nd num:"))
-c = int(input("Enter 3rd num:"))
-print("sum of a+b+c=", a+b+c)
+#a = int(input("Enter 1st num:"))
+#b = int(input("Enter 2nd num:"))
+#c = int(input("Enter 3rd num:"))
+#print("sum of a+b+c=", a+b+c)
+
+
+curnum = int(input("Enter number"))
+print("The next number for the number {} is {}". format(curnum, curnum-1))
+print("The previous number for the number {} is {}". format(curnum, curnum+1))
