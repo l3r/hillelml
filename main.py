831aa9bfe757aa52c786e22b277ffe64521ef982
l3r <sergey.s@myco.com>
2020-02-29 16:29:42 +0200

    task14

--- main.py
+++ main.py
@@ -90,20 +90,20 @@ import numpy
 # print("{}{}{}={}".format(a,op,b,result))
 
 
-print("Enter  a, b ,c")
-a = float(input("a = "))
-b = float(input("b = "))
-c = float(input("c = "))
-
-discr = b ** 2 - 4 * a * c
-print("discr D = %.2f" % discr)
-
-if discr > 0:
-    x1 = (-b + math.sqrt(discr)) / (2 * a)
-    x2 = (-b - math.sqrt(discr)) / (2 * a)
-    print("x1 ={} \nx2 = {}" .format(x1, x2))
-elif discr == 0:
-    x = -b / (2 * a)
-    print("x = {}".format(x))
-else:
-    print("no roots")
+# print("Enter  a, b ,c")
+# a = float(input("a = "))
+# b = float(input("b = "))
+# c = float(input("c = "))
+#
+# discr = b ** 2 - 4 * a * c
+# print("discr D = %.2f" % discr)
+#
+# if discr > 0:
+#     x1 = (-b + math.sqrt(discr)) / (2 * a)
+#     x2 = (-b - math.sqrt(discr)) / (2 * a)
+#     print("x1 ={} \nx2 = {}" .format(x1, x2))
+# elif discr == 0:
+#     x = -b / (2 * a)
+#     print("x = {}".format(x))
+# else:
+#     print("no roots")
