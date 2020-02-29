2ec9a5db1513e534cd307b77b74cbf091c741d24
l3r <sergey.s@myco.com>
2020-02-29 14:01:43 +0200

    task2

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-name = input("Enter Name:")
-print("hello, {}!".format(name))
+#name = input("Enter Name:")
+#print("hello, {}!".format(name))
+
+
+a = int(input("Enter 1st num:"))
+b = int(input("Enter 2nd num:"))
+c = int(input("Enter 3rd num:"))
+print("sum of a+b+c=", a+b+c)
